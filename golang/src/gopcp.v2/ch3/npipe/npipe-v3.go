1a83320e2348d33f8fe596cf5a40dc8f7206482b
hhstore <hhstore@qq.com>
2017-06-11 02:33:21 +0000

    add - <go 并发编程>, 第3章, 重构 io操作代码(最终版). 利用反射reflect, 实现函数多态.
    1. 利用reflect, 实现多态功能.
    2. 当数据变量类型不同, 而行为统一时, 解决API无法统一合并的问题.
    3. 有尝试 函数指针, 自定义 interface{} 等途径, 都无法解决.
    4. 理解go原生不支持多态, 通过反射实现.
    5. 反射本身比较繁琐, 写起来不清晰, 但是可以实现.
    6. 注意通过反射实现多态时, 形参类型: reflect.Value

---
+++ golang/src/gopcp.v2/ch3/npipe/npipe-v3.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"time"
+)
+
+/****************************************************
+
+说明:
+	1. 基于反射reflect, 实现函数的多态(行为相同,参数类型不同)
+	2. 基于reflect各种接口方法, 获取参数, 执行函数调用
+	3. 注意: 通过反射调用函数, 返回结果类型与常规函数调用不同.
+	4. 理解反射的重要作用, 实现对 鸭子类型 变量的支持.
+	5. 运行时, 获取变量类型.
+	6. 当然 API接口的参数类型, 都要定义成 reflect.Value 类型
+	7. reflect包重要接口:
+		- reflect.ValueOf
+		- v.Call(args)
+		- v.MethodByName().Call()
+
+****************************************************/
+
+func main() {
+	//fmt.Println(reflect.TypeOf(os.Pipe))
+	//fmt.Println(reflect.TypeOf(io.Pipe))
+
+	//fmt.Println(reflect.ValueOf(os.Pipe))
+	//fmt.Println(reflect.ValueOf(io.Pipe))
+
+	//v := reflect.ValueOf(os.Pipe)
+	//args := []reflect.Value{}
+	//ret := v.Call(args)
+	//fmt.Println("[kind]", v.Kind(), "\t[Type] ", v.Type())
+	//fmt.Println("[result]", ret)
+
+	// 基于文件io实现:
+	osPipe := reflect.ValueOf(os.Pipe)
+	doPipe(osPipe)
+
+	// 基于内存io实现:
+	ioPipe := reflect.ValueOf(io.Pipe)
+	doPipe(ioPipe)
+
+}
+
+func doPipe(pipeFunc reflect.Value) {
+	var reader, writer reflect.Value
+
+	pipeArgs := []reflect.Value{}      // 空参数, 因为 Pipe()参数为空
+	pipeRet := pipeFunc.Call(pipeArgs) // 执行 Pipe() 方法
+
+	fmt.Println("\n====================\n[pipeResult]:", pipeRet)
+	if len(pipeRet) == 3 { // 基于os.Pipe()
+		reader, writer, _ = pipeRet[0], pipeRet[1], pipeRet[2]
+	} else { // 基于io.Pipe()
+		reader, writer = pipeRet[0], pipeRet[1]
+	}
+
+	go func() {
+		output := make([]byte, 100)
+
+		read := reader.MethodByName("Read")
+
+		read.Call([]reflect.Value{
+			reflect.ValueOf(output),
+		})
+
+		fmt.Println("[output]:", string(output))
+
+	}()
+
+	//----------------------------------------------
+	// 写入操作:
+	//----------------------------------------------
+
+	input := make([]byte, 26)
+
+	for i := 65; i <= 90; i++ {
+		input[i-65] = byte(i)
+	}
+
+	fmt.Println("\t wait data...")
+	time.Sleep(2 * time.Second) // 加入睡眠, 让 读取go例程, 等待数据.
+
+	write := writer.MethodByName("Write")
+
+	result := write.Call([]reflect.Value{
+		reflect.ValueOf(input),
+	})
+
+	_ = result
+
+	//fmt.Println(result)
+
+	time.Sleep(200 * time.Millisecond)
+
+}
