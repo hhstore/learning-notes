a5113a2a033e535345047098883683c28d88769f
hhstore <hhstore@qq.com>
2017-06-13 23:21:29 +0000

    add - <go 并发编程>, 第4章, channel 用法.
    1. channel 的用法:
        - 用法1: 传输业务数据.
    2. 获取 channel 中数据的方式:
        - for() 死循环 + select 方式

---
+++ golang/src/gopcp.v2/ch4/chan/chan0cap/chan0cap.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/****************************************************
+
+        利用 channel 通信
+说明:
+	- 发送 go 例程
+	- 接收部分, 利用 select{}, 实现对 channel 数据收发.
+
+****************************************************/
+
+func main() {
+	sendingInterval := time.Second       // 发送间隔时间
+	receptionInterval := time.Second * 2 // 接收间隔时间
+	intChan := make(chan int, 0)         // 通信 channel, [整型, size = 0, 对比 string, 奇怪?]
+
+	fmt.Println("chan size:", len(intChan)) // ? [size = 0]
+
+	//----------------------------------------------
+	//            向 chan 发送数据
+	//----------------------------------------------
+	go func() {
+		var ts0, ts1 int64
+
+		for i := 1; i <= 5; i++ {
+			intChan <- i // 向 chan 发送数据
+			//
+			ts1 = time.Now().Unix()
+
+			//
+			if ts0 == 0 {
+				fmt.Println("\tSent:", i)
+
+			} else {
+				fmt.Printf("\tSent: %d [interval: %d s]\n", i, ts1-ts0)
+			}
+
+			//
+			ts0 = time.Now().Unix()
+			time.Sleep(sendingInterval)
+		}
+		//
+		close(intChan)
+	}()
+
+	//----------------------------------------------
+	//             从 chan 读取数据
+	//----------------------------------------------
+	var ts0, ts1 int64
+
+Loop:
+	for {
+		//
+		select {
+		// 输出数据:
+		case v, ok := <-intChan: // 从 chan 读取数据
+			if !ok {
+				break Loop
+			}
+
+			ts1 = time.Now().Unix()
+			if ts0 == 0 {
+				fmt.Println("Received:", v) // 输出数据
+
+			} else {
+				fmt.Printf("Received: %d [interval: %d s]\n", v, ts1-ts0) // 输出数据
+			}
+		}
+
+		//
+		ts0 = time.Now().Unix()
+		time.Sleep(receptionInterval) // 接收间隔时间
+	}
+	//
+	fmt.Println("End.")
+
+}
